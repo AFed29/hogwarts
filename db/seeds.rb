415d1380705a205e01d34e9a1b0755f83c13f069
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-02-21 16:31:20 +0000

    Added save method and populated seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+require_relative('../models/student.rb')
+
+
+student1 = Student.new({
+  "first_name" => "Harry",
+  "last_name" => "Potter",
+  "house" => "Griffindor",
+  "age" => 15
+})
+
+student2 = Student.new({
+  "first_name" => "Draco",
+  "last_name" => "Malfoy",
+  "house" => "Slytherin",
+  "age" => 15
+})
+
+student3 = Student.new({
+  "first_name" => "Cedric",
+  "last_name" => "Diggory",
+  "house" => "Hufflepuff",
+  "age" => 17
+})
+
+student1.save()
+student2.save()
+student3.save()
